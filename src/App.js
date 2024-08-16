bee60618916fc32f973097aa72a00b1a76ee8399
Dallas Carlson <118072502+DalPalCarl@users.noreply.github.com>
2024-08-16 14:48:10 -0500

    Updated layout, color, etc.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import Header from "./Header";
-import Footer from "./Footer";
 import './App.css';
-import { Container, Row, Col } from "react-bootstrap";
+import logo from './GWOrgLogo2.png';
 
 function App() {
   return(
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-      <div className="container">
-        <a href="#" className="navbar-brand">Dallas</a>
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container ms-3">
+        <a href="/" className="navbar-brand col"><img src={logo} className="logo" alt="some shit" /></a>
+        <div className='container col'></div>
+        <div className='container col'>
+          <button className='btn '></button>
+        </div>
+        
       </div>
     </nav>
   );
